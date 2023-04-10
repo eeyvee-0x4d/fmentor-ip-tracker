9514ef91a482f2b0785ac7563d3759826eb0a8c6
Al Vincent <alvincentvibar.musa@bicol-u.edu.ph>
2023-04-10 14:56:12 +0800

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import {
   useState,
-  useEffect
+  useEffect,
+  useRef
 } from 'react'
 
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  ZoomControl,
+  useMap
+} from 'react-leaflet'
+
+import L from 'leaflet'
+
+import 'leaflet/dist/leaflet.css';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { faChevronRight, faSpinner } from '@fortawesome/free-solid-svg-icons'
 
 import bgPatternDesktop from '../images/pattern-bg-desktop.png'
 import bgPatternMobile from '../images/pattern-bg-mobile.png'
@@ -14,8 +27,12 @@ import './App.css'
 function App() {
   const [ipAddress, setIpAddress] = useState('')
   const [location, setLocation] = useState('')
+  const [coordinatesLat, setCoordinatesLat] = useState('')
+  const [coordinatesLang, setCoordinatesLang] = useState('')
+  const [mapCenter, setMapCenter] = useState([0, 0])
   const [timeZone, setTimeZone] = useState('')
   const [isp, setIsp] = useState('')
+  const [isLoading, setIsLoading] = useState('false')
 
   const Label = ({title}) => {
     return(
@@ -27,7 +44,7 @@ function App() {
 
   const Details = ({value}) => {
     return(
-      <p className='font-medium text-lg'>
+      <p className='font-medium text-lg pl-4'>
         {value}
       </p>
     )
@@ -35,26 +52,53 @@ function App() {
 
   const API_KEY = import.meta.env.VITE_API_KEY;
   
-  const trackIp = () => {
+  const trackIp = (e) => {
+    e.preventDefault()
     
     const inputValue = document.querySelector('#text-input').value
 
-    let requestUrl = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${inputValue}`
+    let requestUrl = ''
+    const ipv4Regex = /^(\d{1,3}\.){3}\d{1,3}$/
+    const ipv6Regex = /^(?:[A-Fa-f0-9]{1,4}:){7}[A-Fa-f0-9]{1,4}$/
+    const domainRegex = /^(?!:\/\/)(?:(?:(?:xn--)?[a-zA-Z0-9\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF\u200C\u200D-]{1,63}\.)+(?:[a-zA-Z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF\u200C\u200D-]{2,63}|xn--[\w-]{2,59})(?:\s|$))/
+
+    if(ipv4Regex.test(inputValue) || ipv6Regex.test(inputValue)) {
+      requestUrl = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${inputValue}`
+    }
+    else if (domainRegex.test(inputValue)) {
+      requestUrl = requestUrl = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&domain=${inputValue}`
+    }
 
     fetchData(requestUrl)
   }
 
   const fetchData = async (url) => {
+    setIsLoading(true)
     try {
+
       const response = await fetch(url); // Replace with your API endpoint
       const jsonData = await response.json();
+
       setIpAddress(jsonData.ip)
       setLocation(jsonData.location.city)
       setTimeZone(`UTC ${jsonData.location.timezone}`)
       setIsp(jsonData.isp)
+      setCoordinatesLat(jsonData.location.lat)
+      setCoordinatesLang(jsonData.location.lng)
+
+      // const map = useMap()
+      // map.setView([jsonData.location.lat, jsonData.location.lng], 15)
+
     } catch (error) {
-      console.error('Error fetching data:', error);
+      setIpAddress('Error')
+      setLocation('Error')
+      setTimeZone(`Error`)
+      setIsp('Error')
+
+      alert('ERROR: Make sure input is in correct format.')
     }
+    
+    setIsLoading(false)
   };
 
   // useEffect(() => {
@@ -65,10 +109,25 @@ function App() {
     fetchData(`https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}`);
   }, []);
 
+  const mapRef = useRef(null)
+
+  useEffect(() => {
+    setMapCenter([coordinatesLat, coordinatesLang])
+  }, [coordinatesLat, coordinatesLang])
+
+  const customMarkerIcon = new L.Icon({
+    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-black.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
+
   return (
     <div className="App">
       <div className=" relative">
-        <div className='absolute w-full left-1/2 -translate-x-1/2 text-center p-4 flex flex-col justify-center align-center gap-4'>
+        <div className='absolute z-[999] w-full left-1/2 -translate-x-1/2 text-center p-4 flex flex-col justify-center align-center gap-4'>
           <h1 className='font-medium text-white'>
             IP Address Tracker
           </h1>
@@ -80,30 +139,39 @@ function App() {
             </button>
           </div>
           </form>
-          <div className='flex flex-col justify-center items-center gap-4 p-4 bg-white rounded-lg'>
+          <div className='flex flex-col lg:flex-row lg:divide-x justify-center items-center gap-4 p-4 bg-white rounded-lg text-center'>
             <div>
               <Label title="IP ADDRESS"/>
-              <Details value={ipAddress} />
+              {isLoading ? (<FontAwesomeIcon icon={faSpinner} className='animate-spin' />) :( <Details value={ipAddress} />)}
             </div>
             <div>
               <Label title="LOCATION"/>
-              <Details value={location} />
+              {isLoading ? (<FontAwesomeIcon icon={faSpinner} className='animate-spin' />) :( <Details value={location} />)}
             </div>
             <div>
               <Label title="TIMEZONE"/>
-              <Details value={timeZone} />
+              {isLoading ? (<FontAwesomeIcon icon={faSpinner} className='animate-spin' />) :( <Details value={timeZone} />)}
             </div>
             <div>
               <Label title="ISP"/>
-              <Details value={isp} />
+              {isLoading ? (<FontAwesomeIcon icon={faSpinner} className='animate-spin' />) :( <Details value={isp} />)}
             </div>
           </div>
         </div>
-        <picture>
-          <source srcSet={bgPatternDesktop} media="(min-width: 768px)"/>
+        <picture className='w-screen'>
+          <source srcSet={bgPatternDesktop} media="(min-width: 375px)"/>
           <img src={bgPatternMobile} alt="background pattern" />
         </picture>
       </div>
+      <MapContainer center={mapCenter} zoom={13} zoomControl={false} scrollWheelZoom={false} className='min-h-[500px] h-full overflow-hidden'>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <ZoomControl position="bottomleft" />
+        <Marker position={mapCenter} icon={customMarkerIcon}>
+        </Marker>
+      </MapContainer>
     </div>
   )
 }
